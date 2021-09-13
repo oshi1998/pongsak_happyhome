409b5e11fd30bc96fe18e84b293f8998ab90ea28
oshi1998 <76892994+oshi1998@users.noreply.github.com>
2021-09-14 01:04:54 +0700

    version 1.1.0

--- services/uploadImage_roomtype.php
+++ services/uploadImage_roomtype.php
@@ -8,7 +8,7 @@ if ($_FILES['file']['tmp_name'] && isset($_SESSION['mrt_id'])) {
     
     $ext = end(explode(".", $_FILES['file']['name']));
     $filename = uniqid() . "." . $ext;
-    $img_path = $_SERVER['DOCUMENT_ROOT']."/pongsak_happyhome/assets/img/roomtypes/other/" . $filename;
+    $img_path = "../assets/img/roomtypes/other/".$filename;
 
     try {
         move_uploaded_file($_FILES['file']['tmp_name'],$img_path);
